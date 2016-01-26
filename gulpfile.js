cb4f6094cf6793f37a8c399aa1f4432e1af80c07
Tommy Quissens <tommy.quissens@student.odisee.be>
2016-01-26 15:51:15 +0100

    ADD: gulp with sass, sourcemaps, autoprefixer, livereload and rename

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    rename = require('gulp-rename');
+
+var input = './scss/**/*.scss',
+    output = './public/stylesheets',
+    sassOptions = {
+        errLogToConsole: true,
+        outputStyle: 'compressed'
+    },
+    autoprefixerOptions = {
+        browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    };
+gulp.task('sass', function () {
+    return gulp
+        .src(input)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(output))
+        .pipe(livereload())
+        //.pipe(sassdoc())
+        // Release the pressure back and trigger flowing mode (drain)
+        // See: http://sassdoc.com/gulp/#drain-event
+        .resume();
+});
+gulp.task('watch', function () {
+    livereload.listen();
+    return gulp
+        .watch(input, ['sass'])
+        //.on('change', function(event){
+        //
+        //});
+});
+gulp.task('default', ['sass', 'watch']);
