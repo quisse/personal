418e02e887a48b33feab72812ca3d1530f7f68da
Tommy Quissens <tommy.quissens@student.odisee.be>
2016-01-27 17:17:28 +0100

    FIX: gulp now checks sass and js code & reloads when necessary ADD: about route ADD: keepalive path

--- gulpfile.js
+++ gulpfile.js
@@ -2,40 +2,57 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
-    livereload = require('gulp-livereload'),
-    rename = require('gulp-rename');
+    //livereload = require('gulp-livereload'),
+    rename = require('gulp-rename'),
+    server = require('gulp-express');
 
-var input = './scss/style.scss',
-    target = './scss/**/*.scss';
-    output = './public/stylesheets',
-    sassOptions = {
-        errLogToConsole: true,
-        outputStyle: 'compressed'
+var sassConfig = {
+        input: './scss/style.scss',
+        target: './scss/**/*.scss',
+        output: './public/stylesheets',
+        sassOptions: {
+            errLogToConsole: true,
+            outputStyle: 'compressed'
+        },
+        autoprefixerOptions: {
+            browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+        }
     },
-    autoprefixerOptions = {
-        browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    expressConfig = {
+        source : 'bin/www',
+        js:['views/**/*','routes/**/*.js']
     };
+
 gulp.task('sass', function () {
     return gulp
-        .src(input)
+        .src(sassConfig.input)
         .pipe(sourcemaps.init())
-        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sass(sassConfig.sassOptions).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(autoprefixer(sassConfig.autoprefixerOptions))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(output))
-        .pipe(livereload())
+        .pipe(gulp.dest(sassConfig.output))
+        //.pipe(livereload())
         //.pipe(sassdoc())
         // Release the pressure back and trigger flowing mode (drain)
         // See: http://sassdoc.com/gulp/#drain-event
         .resume();
 });
-gulp.task('watch', function () {
-    livereload.listen();
-    return gulp
-        .watch(target, ['sass']);
-        //.on('change', function(event){
-        //
-        //});
+
+//gulp.task('watch', function () {
+//    livereload.listen();
+//    return gulp
+//        .watch(sassConfig.target, ['sass']);
+//});
+
+gulp.task('server', function () {
+    server.run([expressConfig.source]);
+
+    gulp.watch([sassConfig.target], ['sass']);
+    gulp.watch([sassConfig.output+'/**/*.css'], server.notify);
+    gulp.watch(expressConfig.js, function(event){
+        server.run([expressConfig.source]);
+        server.notify(event)
+    });
 });
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'server']);
