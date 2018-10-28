c4184ffb731dea577c1f9bd3bb1699f2420ea02b
Tommy Quissens <tommy.quissens@storefront.be>
2018-10-28 18:25:02 +0100

    attempting to make the encrypted file work
    more fixing
    
    more fixing
    
    debugging
    
    more debugging
    
    more debugging
    
    more debugging
    
    debugging
    
    debugging
    
    debugging
    
    testing
    
    testing
    
    testing
    
    testing
    
    testing
    
    testing
    
    testing
    
    dsaf
    
    test
    
    testing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ls -al .travis
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
